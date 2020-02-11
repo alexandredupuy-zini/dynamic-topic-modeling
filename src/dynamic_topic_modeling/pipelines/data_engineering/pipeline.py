325e5f7e6829b628bfebfab8e1f0f37c00120021
Gaspard Michel <gaspard.michel@ensae.fr>
2020-02-11 21:56:08 +0100

    Modified pipeline.py. Disabled the UN dataset as an input but added the possibility to load the dataset from catalog in the preprocess function (in node.py)

--- src/dynamic_topic_modeling/pipelines/data_engineering/pipeline.py
+++ src/dynamic_topic_modeling/pipelines/data_engineering/pipeline.py
@@ -1,20 +1,22 @@
 from kedro.pipeline import Pipeline, node
 from dynamic_topic_modeling.pipelines.data_engineering.nodes import (
-    preprocess_UNGD,
+	preprocess_UNGD,
 )
-
+from kedro.io.core import DataSetError
 def create_pipeline(**kwargs):
-    return Pipeline(
-        [
-            node(
-                func=preprocess_UNGD,
-                inputs=["UN_dataset", "params:extreme_no_below", "params:extreme_no_above", "params:enable_bigram", "params:min_bigram_count"],
-                outputs=dict(
-                    UNGD_preprocessed="UNGD",
-                    corpus="corpus",
-                    dictionnary="dictionnary",
-                ),
-                name="UNGD_preprocessing",
-            )
-        ]
-    )
+
+		return Pipeline(
+			[
+				node(
+					func=preprocess_UNGD,
+					inputs=["params:extreme_no_below", "params:extreme_no_above", "params:enable_bigram", "params:min_bigram_count","params:download_dataset",'params:basic_word_analysis'],
+					outputs=dict(
+						UNGD_preprocessed="UNGD",
+						corpus="corpus",
+						dictionnary="dictionnary",
+					),
+					name="UNGD_preprocessing",
+				)
+			]
+		)
+		
