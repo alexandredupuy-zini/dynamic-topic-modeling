c9f6754fc6b38b45eb60d7ea9f1dfd11cffbdd8d
Gaspard Michel <gaspard.michel@ensae.fr>
2020-02-11 22:40:04 +0100

    Changed output names to similar catalog names

--- src/dynamic_topic_modeling/pipelines/data_engineering/pipeline.py
+++ src/dynamic_topic_modeling/pipelines/data_engineering/pipeline.py
@@ -2,7 +2,6 @@ from kedro.pipeline import Pipeline, node
 from dynamic_topic_modeling.pipelines.data_engineering.nodes import (
 	preprocess_UNGD,
 )
-from kedro.io.core import DataSetError
 def create_pipeline(**kwargs):
 
 		return Pipeline(
@@ -11,9 +10,9 @@ def create_pipeline(**kwargs):
 					func=preprocess_UNGD,
 					inputs=["params:extreme_no_below", "params:extreme_no_above", "params:enable_bigram", "params:min_bigram_count","params:download_dataset",'params:basic_word_analysis'],
 					outputs=dict(
-						UNGD_preprocessed="UNGD",
-						corpus="corpus",
-						dictionnary="dictionnary",
+						UNGD_preprocessed="UN_preprocessed",
+						corpus="UN_corpus",
+						dictionnary="UN_dictionary:",
 					),
 					name="UNGD_preprocessing",
 				)
